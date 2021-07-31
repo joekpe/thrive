5f82196532c5ddba91e83117ae67cbec0744db7e
Frederick Amoako-Atta <amoakofredatta@gmail.com>
2021-07-31 19:10:29 +0000

    Fix 2

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        TCG\Voyager\VoyagerServiceProvider::class
+        TCG\Voyager\VoyagerServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
